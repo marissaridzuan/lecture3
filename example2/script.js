b7c8055dddfd676d6cc0cf9e15881274ed12ad52
marissaridzuan <77322282+marissaridzuan@users.noreply.github.com>
2021-01-27 22:23:49 +0800

    Test

--- example2/script.js
+++ example2/script.js
@@ -64,10 +64,9 @@ async function compute() {
     trees.push(param2)
 
     // Call RhinoCompute
-
     const res = await RhinoCompute.Grasshopper.evaluateDefinition(definition, trees)
 
-    console.log(res) 
+    // console.log(res) 
 
     collectResults(res.values)
 
@@ -163,7 +162,7 @@ function init() {
 
     // create a scene and a camera
     scene = new THREE.Scene()
-    scene.background = new THREE.Color(1, 1, 1)
+    scene.background = new THREE.Color(1,1,1)
     camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
     camera.position.z = - 30
 
